134724caef7356199e90dbe089ea252df4086812
Giovani <gioelo@hotmail.com>
2024-12-04 12:12:00 -0800

    Cambios de animacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegistroComponent } from './registro/registro.component';
+import { CommonModule } from '@angular/common';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RegistroComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    RegistroComponent
+  ],
+  providers: [
+    provideAnimations() 
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'registro';
